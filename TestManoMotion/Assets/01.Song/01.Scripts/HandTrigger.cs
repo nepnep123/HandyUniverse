b713cc3628cf7c646ec1ece38510dee4a4ce2f16
SongYounghoon <nepnep123@naver.com>
2019-09-25 15:43:06 +0900

    ADD MANOMOTION, ARCORE
    2019.09.25
    1. 기본적인 셋팅 구현
    2. HandTrigger
    3. 공용 임포트 폴더에 UI 임포트

---
+++ TestManoMotion/Assets/01.Song/01.Scripts/HandTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTrigger : MonoBehaviour
+{
+	private void Update()
+	{
+		Movement(ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info);
+	}
+
+
+	//HandTrigger
+	void Movement(TrackingInfo trackingInfo)
+	{
+		Vector3 normalizedPalmCenterPosition = trackingInfo.palm_center;
+		float depth = trackingInfo.relative_depth;
+
+		Vector3 relativePalmCenterPosition = ManoUtils.Instance.CalculateNewPosition(normalizedPalmCenterPosition, depth);
+
+		float smoothingVariable = 0.85f;
+		transform.position = Vector3.Lerp(transform.position, relativePalmCenterPosition, smoothingVariable);
+	}
+}
