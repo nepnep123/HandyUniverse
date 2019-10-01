683a8a4f6d69fe457d83f038380fa523fe7d9327
sonic817 <hsyle817@gmail.com>
2019-10-01 17:41:09 +0900

    1001 1740 이강원
    푸시

---
+++ TestManoMotion/Assets/03.Kang/02.Scripts/ARManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using System;
+
+#if UNITY_EDITOR
+using input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class ARManager : MonoBehaviour
+{
+    public GameObject model;
+    public GameObject skyboxCamera;
+    public GameObject quad;
+    public GameObject planeVisualizer;
+
+    void Awake()
+    {
+        skyboxCamera.SetActive(false);
+        quad.SetActive(false);
+    }
+
+    void Update()
+    {
+        MakeModel();
+    }
+
+    private void MakeModel()
+    {
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began && Input.touchCount > 0)
+        {
+            TrackableHit hit;
+            TrackableHitFlags flags = TrackableHitFlags.FeaturePointWithSurfaceNormal | TrackableHitFlags.PlaneWithinPolygon;
+
+            if (Frame.Raycast(touch.position.x, touch.position.y, flags, out hit))
+            {
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                GameObject modelModel = Instantiate(model, hit.Pose.position, hit.Pose.rotation, anchor.transform);
+
+                skyboxCamera.SetActive(true);
+                quad.SetActive(true);
+
+                //Destroy(planeVisualizer); // 더 이상 바닥인식은 NO
+            }
+        }
+    }
+}
+//private void MakeSomthing()
+//{
+//    Touch touch = Input.GetTouch(0);
+//    touchPosition = touch.position;
+
+//    if (Input.touchCount > 0 && touch.phase == TouchPhase.Began)
+//    {
+//        if (raycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+//        {
+//            Pose hitPose = hits[0].pose;
+//            GameObject modelModel = Instantiate(model, hitPose.position, hitPose.rotation);
+//        }
+//    }
+//}
---
+++ TestManoMotion/Assets/03.Kang/02.Scripts/PortalWindow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalWindow : MonoBehaviour
+{
+    public Material[] materials;
+
+    private Transform device;
+    private GameObject device1;
+
+    private bool hasCollided;
+
+    private void Awake()
+    {
+        device1 = GameObject.Find("ARCore Device");
+        device = device1.transform;
+    }
+
+    void Start()
+    {
+        SetMaterials(false);
+    }
+
+    private void SetMaterials(bool fullRender)
+    {
+        var stencilTest = fullRender ? CompareFunction.NotEqual : CompareFunction.Equal;
+
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest", (int)stencilTest);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform != device) return;
+
+        hasCollided = !hasCollided;
+
+        SetMaterials(hasCollided);
+    }
+}
