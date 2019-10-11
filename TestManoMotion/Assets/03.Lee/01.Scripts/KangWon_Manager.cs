0cc24b36de45ce04c3d332f8643a42a7b71440a8
sonic817 <hsyle817@gmail.com>
2019-10-11 18:48:46 +0900

    a
    a

---
+++ TestManoMotion/Assets/03.Lee/01.Scripts/KangWon_Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KangWon_Manager : MonoBehaviour
+{
+    /*
+     
+    ■ 강원
+    1. 영훈씨에게 장소에 대한 정보(함수나 인자나 등)를 전달받아서(게임매니저를 만들되 싱글턴으로)
+    2. 어느 행성인지를 판단해서 행성에 따른 장소에 사진을 저장시키는 로직 구현(PhotoUtilie)
+    3. 사진 찍고 이후에 또 찍어도 저장되게끔
+    
+        
+    ■ 영훈
+    행성에 들어가는 것까지 구현
+    
+     */
+}
