329e55d372344ce856eaa1e12cda6fef8b086661
SongYounghoon <nepnep123@naver.com>
2019-10-17 20:15:46 +0900

    2019.10.17
    111

---
+++ TestManoMotion/Assets/01.Song/01.Scripts/00.Common/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	public static UIManager instance;
+
+	public GameObject UIInfo_img;
+	public Text content_txt;
+
+	private void Awake()
+	{
+		if (instance == null) instance = GetComponent<UIManager>();
+		else Destroy(this);
+	}
+
+	public IEnumerator ShowInfoUI(string content)
+	{
+
+		content_txt.text = content;
+
+		//왼쪽 바 나오고 들어감
+		iTween.MoveTo(UIInfo_img, iTween.Hash("x", 25, "time", 3.0f, "easetype", iTween.EaseType.easeOutQuad));
+		yield return new WaitForSeconds(5.0f);
+		iTween.MoveTo(UIInfo_img, iTween.Hash("x", -500, "time", 3.0f, "easetype", iTween.EaseType.easeOutQuad));
+	}
+
+}
---
+++ TestManoMotion/Assets/01.Song/01.Scripts/The Moon/MoonUICtrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoonUICtrl : MonoBehaviour
+{
+
+	private void Start()
+	{
+		StartCoroutine(UIManager.instance.ShowInfoUI("씨발러마"));
+	}
+
+
+}
