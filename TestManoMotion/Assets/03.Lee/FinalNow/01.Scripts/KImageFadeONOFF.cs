b78d92462c9fe7395faf7e0de59cbc32cc618b09
sonic817 <hsyle817@gmail.com>
2019-11-01 19:26:19 +0900

    a
    a

---
+++ TestManoMotion/Assets/03.Lee/FinalNow/01.Scripts/KImageFadeONOFF.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KImageFadeONOFF : MonoBehaviour
+{
+    // 두번째 이미지에만 스크립트 붙이면 됨
+
+    public Image imageImage1;
+    public Image imageImage2;
+
+    private void Start()
+    {
+        StartCoroutine(FadeOff());
+    }
+
+    IEnumerator FadeOff()
+    {
+        yield return new WaitForSeconds(1f);
+
+        imageImage1.enabled = false;
+        imageImage2.enabled = true;
+        
+        StartCoroutine(FadeOn());
+    }
+
+    IEnumerator FadeOn()
+    {
+        yield return new WaitForSeconds(1f);
+
+        imageImage1.enabled = true;
+        imageImage2.enabled = false;
+
+        StartCoroutine(FadeOff());
+    }
+}
